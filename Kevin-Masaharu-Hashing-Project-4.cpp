650c211fbd2959369c1beaac67fc291e809600b9
KevinHendersonCSUF <123698269+KevinHendersonCSUF@users.noreply.github.com>
2024-05-16 21:54:04 +0000

    Created header, got file started

--- Kevin-Masaharu-Hashing-Project-4.cpp
+++ Kevin-Masaharu-Hashing-Project-4.cpp
@@ -1,1 +1,45 @@
+// Name: Kevin Henderson
+// CWID: 885395855
+// Git repository Link:
+// https://github.com/KevinHendersonCSUF/Kevin-Masaharu-Hashing-Project-4.git
 
+// Name: Masaharu Saito
+// CWID:
+// Git repository Link:
+// https://github.com/KevinHendersonCSUF/Kevin-Masaharu-Hashing-Project-4.git
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class Hash {
+public:
+int div_hash(int key, int M) {
+
+    return 0;
+}
+
+int mid_sqr_hash(int key, int  M) {
+
+    return 0;
+}
+
+int double_hash (int key, int M) {
+
+    return 0;
+}
+
+int mult_hash(int key, int A) {
+
+    return 0;
+}
+private: 
+int key_ = 0;
+int M = 0;
+int A = 0;
+
+};
+
+void main(){
+
+};
